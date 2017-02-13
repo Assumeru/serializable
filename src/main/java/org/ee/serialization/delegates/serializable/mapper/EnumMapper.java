b3ca3bc5729257e920aa5e9cdc3aa838097c6018
Assumeru <malusluminis@hotmail.com>
2017-02-13 22:42:24 +0100

    enums

--- src/main/java/org/ee/serialization/delegates/serializable/mapper/EnumMapper.java
+++ src/main/java/org/ee/serialization/delegates/serializable/mapper/EnumMapper.java
@@ -1,38 +1,39 @@
 package org.ee.serialization.delegates.serializable.mapper;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.Map;
 import java.util.WeakHashMap;
 
-import org.ee.serialization.delegates.serializable.mapper.model.ArrayMapping;
+import org.ee.serialization.SerializationException;
 import org.ee.serialization.delegates.serializable.mapper.model.ClassDescription;
+import org.ee.serialization.delegates.serializable.mapper.model.EnumMapping;
 import org.ee.serialization.delegates.serializable.mapper.model.ObjectMapping;
 
-public class ArrayMapper implements ObjectInputStreamMapperDelegate {
+public class EnumMapper implements ObjectInputStreamMapperDelegate {
 	private final Map<ClassDescription, Boolean> mappable;
 
-	public ArrayMapper() {
+	public EnumMapper() {
 		mappable = new WeakHashMap<>();
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public Object map(Object object, ObjectInputStreamMapper mapper) throws IOException, ClassNotFoundException {
-		if(object instanceof ArrayMapping) {
-			ArrayMapping mapping = (ArrayMapping) object;
-			Class<?> type = Class.forName(mapping.getDescription().getName());
-			Object out = Array.newInstance(type.getComponentType(), mapping.size());
-			for(int i = 0; i < mapping.size(); i++) {
-				Array.set(out, i, mapper.map(mapping.get(i), mapper));
+		if(object instanceof EnumMapping) {
+			EnumMapping mapping = (EnumMapping) object;
+			Class type = Class.forName(mapping.getDescription().getName());
+			try {
+				return Enum.valueOf(type, mapping.getName());
+			} catch(IllegalArgumentException e) {
+				throw new SerializationException("Failed to deserialize", e);
 			}
-			return out;
 		}
 		return object;
 	}
 
 	@Override
 	public boolean canMap(Object object) {
-		if(object instanceof ArrayMapping) {
+		if(object instanceof EnumMapping) {
 			ClassDescription description = ((ObjectMapping) object).getDescription();
 			Boolean canBeMapped = mappable.get(description);
 			if(canBeMapped != null) {
